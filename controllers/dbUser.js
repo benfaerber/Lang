5747d5c7cf4df275786934438ae89503b66dba63
Ben Faerber <benfaerber@protonmail.com>
2020-03-28 18:18:46 -0600

    Added city db

--- controllers/dbUser.js
+++ controllers/dbUser.js
@@ -1,48 +1,49 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
-module.exports = (db) => {
-  var uc = db.collection('users');
-  var module = {};
+module.exports = db => {
+	var uc = db.collection('users');
+	var module = {};
 
-  module.getAllUsers = async () => {
-    let result = await uc.find({}).toArray();
-    return result.map(user => new User(user));
-  };
-  
-  module.getUser = async uname => new User(await uc.findOne({ username: uname }));
-  
-  module.getPassword = async uname => {
-    let result = await uc.findOne({ username: uname });
-    let pass = result ? result.password : null;
-    return pass;
-  };
-  
-  module.createUser = async (user, password) => {
-    obj = user.toSave();
-    obj.password = await bcrypt.hash(password, 10);
-    uc.insertOne(obj);
-    console.log(`User '${user.username}' was created.`);
-  };
-  
-  module.changePassword = async (user, newPassword) => {
-    let hashed = await bcrypt.hash(newPassword, 10);
-    await uc.updateOne(
-      { username: user.username },
-      { $set: { password: hashed } }
-    );
-    console.log(`User '${user.username}' changed their password.`);
-  };
-  
-  // returns boolean if auth passes
-  module.checkPassword = async (username, testPass) => {
-    let userPass = await getPassword(username);
-    if (!userPass) {
-      return false;
-    }
-  
-    return await bcrypt.compare(testPass, userPass);
-  };
+	module.getAllUsers = async () => {
+		let result = await uc.find({}).toArray();
+		return result.map(user => new User(user));
+	};
 
-  return module;
-}
+	module.getUser = async uname =>
+		new User(await uc.findOne({ username: uname }));
+
+	module.getPassword = async uname => {
+		let result = await uc.findOne({ username: uname });
+		let pass = result ? result.password : null;
+		return pass;
+	};
+
+	module.createUser = async (user, password) => {
+		obj = user.toSave();
+		obj.password = await bcrypt.hash(password, 10);
+		uc.insertOne(obj);
+		console.log(`User '${user.username}' was created.`);
+	};
+
+	module.changePassword = async (user, newPassword) => {
+		let hashed = await bcrypt.hash(newPassword, 10);
+		await uc.updateOne(
+			{ username: user.username },
+			{ $set: { password: hashed } }
+		);
+		console.log(`User '${user.username}' changed their password.`);
+	};
+
+	// returns boolean if auth passes
+	module.checkPassword = async (username, testPass) => {
+		let userPass = await getPassword(username);
+		if (!userPass) {
+			return false;
+		}
+
+		return await bcrypt.compare(testPass, userPass);
+	};
+
+	return module;
+};
--- controllers/mongo.js
+++ controllers/mongo.js
@@ -2,7 +2,7 @@ let db;
 
 const mongo = require('mongodb').MongoClient;
 const connectionString = 'mongodb://localhost:27017/';
-let dbUser;
+let dbUser, dbCities;
 
 (async () => {
 	let client = await mongo.connect(connectionString, {
@@ -12,11 +12,11 @@ let dbUser;
 	db = client.db('node');
 	// Import modules here:
 	dbUser = require('./dbUser')(db);
+	dbCities = require('./dbCities')(db);
 
 	await playground();
 })().catch(err => console.error(err));
 
 playground = async () => {
-	let user = await dbUser.getUser('liltim');
-	console.log(user);
+	await dbCities.playground();
 };
